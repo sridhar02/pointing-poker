686b931a191f98ff7f63a9a7eef66b3113a19e6d
sridhar02 <kattasridhar02@gmail.com>
2024-12-28 13:30:27 +0530

    sentry config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,12 +5,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://2d5901f9b7a127ba61b7077464d3597a@o4506222590230528.ingest.us.sentry.io/4508535666442240",
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://2d5901f9b7a127ba61b7077464d3597a@o4506222590230528.ingest.us.sentry.io/4508535666442240",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
